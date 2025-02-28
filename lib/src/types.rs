682af15bcc6ae425042a4b0600ff6a5942edde59
Vincent Grossier <vincent.grossier@gmail.com>
2025-02-27 20:34:54 -0500

    add MerkleRoot implementation + derivations

--- lib/src/types.rs
+++ lib/src/types.rs
@@ -1,8 +1,12 @@
-use std::process::Output;
-
-use crate::U256;
+use crate::{
+    crypto::{PublicKey, Signature},
+    U256,
+};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Blockchain {
     pub blocks: Vec<Block>,
 }
@@ -15,6 +19,7 @@ impl Blockchain {
     }
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Block {
     pub header: BlockHeader,
     pub transactions: Vec<Transaction>,
@@ -31,9 +36,10 @@ impl Block {
     }
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct BlockHeader {
     /// Timestamp of the block
-    pub timestamp: u64,
+    pub timestamp: DateTime<Utc>,
     /// Nonce used to mine the block
     pub nonce: u64,
     /// Hash of the previous block
@@ -45,7 +51,7 @@ pub struct BlockHeader {
 }
 impl BlockHeader {
     pub fn new(
-        timestamp: u64,
+        timestamp: DateTime<Utc>,
         nonce: u64,
         prev_block_hash: [u8; 32],
         merkle_root: [u8; 32],
@@ -64,6 +70,8 @@ impl BlockHeader {
         unimplemented!()
     }
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Transaction {
     pub inputs: Vec<TransactionInput>,
     pub outputs: Vec<TransactionOutput>,
@@ -77,13 +85,15 @@ impl Transaction {
     }
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TransactionInput {
     pub prev_transaction_output_hash: [u8; 32],
-    pub signature: [u8; 64], // dummy tpes, will be replaced later
+    pub signature: Signature, // dummy types, will be replaced later
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TransactionOutput {
     pub value: u64,
     pub unique_ud: Uuid,
-    pub pubkey: [u8; 33], // dummy tpes, will be replaced later
+    pub pubkey: PublicKey, // dummy types, will be replaced later
 }
